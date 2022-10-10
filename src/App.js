70bffa2d8e176634966ca9bd6b30630ac8b76939
Asutosh Acharya <asutosh246@gmail.com>
2022-10-10 21:17:24 +0530

    Adding Validations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Confirmation from "./components/ConfirmationPage/Confirmation";
 import FeedbackForm from "./components/FeedbackForm/FeedbackForm";
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <FeedbackForm /> */}
-      <Confirmation/>
+      <Routes>
+        <Route path="/" element={<FeedbackForm />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
     </div>
   );
 }
--- src/components/FeedbackForm/FeedbackForm.js
+++ src/components/FeedbackForm/FeedbackForm.js
@@ -1,58 +1,144 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FeedbackForm.css";
-// import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { useNavigate } from "react-router-dom";
 const FeedbackForm = (props) => {
+  let name, value;
+
+  const [data, setData] = useState({
+    name: "",
+    phone: "",
+    qualityOfService: "",
+    cleanliness: "",
+    email: "",
+    beverage: "",
+    diningExperience: "",
+  });
+
+  const handleInputs = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+
+    setData({ ...data, [name]: value });
+  };
+
+  const navigate = useNavigate();
+
+  const pageChangeHandler = () => {
+    if (
+      data.name !== "" &&
+      data.phone !== "" &&
+      data.qualityOfService !== "" &&
+      data.cleanliness !== "" &&
+      data.email !== "" &&
+      data.beverage !== "" &&
+      data.diningExperience !== ""
+    ) {
+      navigate("/Confirmation");
+    }
+  };
+
   return (
     <div>
       <header>
         <h1>Aromatic Bar</h1>
-        {/* <div className="ico"><CheckCircleIcon className="c1"/></div> */}
       </header>
       <form className="review-form">
         <div className="form left">
           <label htmlFor="name">
             Customer Name<sup>*</sup>
           </label>
-          <input type="text" placeholder="E.g.Jon Snow" required />
+          <input
+            name="name"
+            type="text"
+            placeholder="E.g.Jon Snow"
+            required
+            value={data.name}
+            onChange={handleInputs}
+          />
           <label htmlFor="phone">
             Phone<sup>*</sup>
           </label>
-          <input type="number" required placeholder="9999999999" />
-          <label htmlFor="quality">
+          <input
+            name="phone"
+            type="number"
+            required
+            placeholder="9999999999"
+            value={data.phone}
+            onChange={handleInputs}
+          />
+          <label htmlFor="qualityOfService">
             Please rate the quality of service you recived from your host.
             <sup>*</sup>
           </label>
           <div>
             <input
               type="radio"
-              name="quality"
-              value={"Excellent"}
+              name="qualityOfService"
               id="quality"
+              value={"Excellent"}
+              onChange={handleInputs}
             />
             <span>Excellent</span>
-            <input type="radio" name="quality" id="quality" value={"Good"} />
+            <input
+              type="radio"
+              name="qualityOfService"
+              id="quality"
+              value={"Good"}
+              onChange={handleInputs}
+            />
             <span>Good</span>
-            <input type="radio" name="quality" value={"Fair"} id="quality" />
+            <input
+              type="radio"
+              name="qualityOfService"
+              id="quality"
+              value={"Fair"}
+              onChange={handleInputs}
+            />
             <span>Fair</span>
-            <input type="radio" name="quality" value={"Bad"} id="quality" />
+            <input
+              type="radio"
+              name="qualityOfService"
+              id="quality"
+              value={"Bad"}
+              onChange={handleInputs}
+            />
             <span>Bad</span>
           </div>
-          <label htmlFor="service">
+          <label htmlFor="cleanliness">
             Was our restaurant clean?<sup>*</sup>
           </label>
           <div>
             <input
               type="radio"
-              name="service"
+              name="cleanliness"
+              id="cleanliness"
               value={"Excellent"}
-              id="service"
+              onChange={handleInputs}
             />
             <span>Excellent</span>
-            <input type="radio" name="service" id="service" value={"Good"} />
+            <input
+              type="radio"
+              name="cleanliness"
+              id="cleanliness"
+              value={"Good"}
+              onChange={handleInputs}
+            />
             <span>Good</span>
-            <input type="radio" name="service" value={"Fair"} id="service" />
+            <input
+              type="radio"
+              name="cleanliness"
+              id="cleanliness"
+              value={"Fair"}
+              onChange={handleInputs}
+            />
             <span>Fair</span>
-            <input type="radio" name="service" value={"Bad"} id="service" />
+            <input
+              type="radio"
+              name="cleanliness"
+              id="cleanliness"
+              value={"Bad"}
+              onChange={handleInputs}
+            />
             <span>Bad</span>
           </div>
         </div>
@@ -60,7 +146,14 @@ const FeedbackForm = (props) => {
           <label htmlFor="email">
             Email<sup>*</sup>
           </label>
-          <input type="email" required placeholder="E.g:abc@gmail.com" />
+          <input
+            name="email"
+            type="email"
+            required
+            placeholder="E.g:abc@gmail.com"
+            value={data.email}
+            onChange={handleInputs}
+          />
           <label htmlFor="beverage">
             please rate the quality of beverage.<sup>*</sup>
           </label>
@@ -68,52 +161,81 @@ const FeedbackForm = (props) => {
             <input
               type="radio"
               name="beverage"
-              value={"Excellent"}
               id="beverage"
+              value={"Excellent"}
+              onChange={handleInputs}
             />
             <span>Excellent</span>
-            <input type="radio" name="beverage" id="beverage" value={"Good"} />
+            <input
+              type="radio"
+              name="beverage"
+              id="beverage"
+              value={"Good"}
+              onChange={handleInputs}
+            />
             <span>Good</span>
-            <input type="radio" name="beverage" value={"Fair"} id="beverage" />
+            <input
+              type="radio"
+              name="beverage"
+              id="beverage"
+              value={"Fair"}
+              onChange={handleInputs}
+            />
             <span>Fair</span>
-            <input type="radio" name="beverage" value={"Bad"} id="beverage" />
+            <input
+              type="radio"
+              name="beverage"
+              id="beverage"
+              value={"Bad"}
+              onChange={handleInputs}
+            />
             <span>Bad</span>
           </div>
-          <label htmlFor="experience">
+          <label htmlFor="diningExperience">
             Please rate your dining experience.<sup>*</sup>
           </label>
           <div>
             <input
               type="radio"
-              name="experience"
-              value={"Excellent"}
+              name="diningExperience"
               id="experience"
+              value={"Excellent"}
+              onChange={handleInputs}
             />
             <span>Excellent</span>
             <input
               type="radio"
-              name="experience"
+              name="diningExperience"
               id="experience"
               value={"Good"}
+              onChange={handleInputs}
             />
             <span>Good</span>
             <input
               type="radio"
-              name="experience"
-              value={"Fair"}
+              name="diningExperience"
               id="experience"
+              value={"Fair"}
+              onChange={handleInputs}
             />
             <span>Fair</span>
             <input
               type="radio"
-              name="experience"
-              value={"Bad"}
+              name="diningExperience"
               id="experience"
+              value={"Bad"}
+              onChange={handleInputs}
             />
             <span>Bad</span>
           </div>
           <div className="btn">
-            <button type="submit" className="button">Submit Review</button>
+            <button
+              type="submit"
+              className="button"
+              onClick={pageChangeHandler}
+            >
+              Submit Review
+            </button>
           </div>
         </div>
       </form>
