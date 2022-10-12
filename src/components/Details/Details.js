c6df5e19b28180ec7fe0b8ae4530933d3f02bb35
Asutosh Acharya <asutosh246@gmail.com>
2022-10-12 11:42:18 +0530

    using material table

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,7 +1,9 @@
-import { TableHead } from "@mui/material";
+// import { TableHead } from "@mui/material";
 import React from "react";
-import { useMemo } from "react";
-import { useTable } from "react-table";
+// import { useMemo } from "react";
+// import { useTable } from "react-table";
+// import MaterialTable from '@mui/icons-material';
+
 function Details() {
   function allStorage() {
     var values = [],
@@ -18,51 +20,42 @@ function Details() {
   console.log(data);
   console.log(data.length);
 
-  const columns = useMemo(() => {
-    return [
-      {
-        header: "formDetails",
-        accessor: "view details",
-      },
-      {
-        header: "Customer Name",
-        accessor: "name",
-      },
-      {
-        header: "Email",
-        accessor: "email",
-      },
-      {
-        header: "Phone",
-        accessor: "phone",
-      },
-      {
-        header: "Please rate the quality of service you recived from host",
-        accessor: "qualityOfService",
-      },
-      {
-        header: "Please rate the quality of beverage",
-        accessor: "beverage",
-      },
-      {
-        header: "Please rate the overall dining experience",
-        accessor: "diningExperience",
-      },
-      {
-        header: "Was our restaurant clean?",
-        accessor: "cleanliness",
-      },
-    ];
-  });
+  const columns = [
+    
+    {
+      title: "Customer Name",
+      field: "name",
+    },
+    {
+      title: "Email",
+      field: "email",
+    },
+    {
+      title: "Phone",
+      field: "phone",
+    },
+    {
+      title: "Please rate the quality of service you recived from host",
+      field: "qualityOfService",
+    },
+    {
+      title: "Please rate the quality of beverage",
+      field: "beverage",
+    },
+    {
+      title: "Please rate the overall dining experience",
+      field: "diningExperience",
+    },
+    {
+      title: "Was our restaurant clean?",
+      field: "cleanliness",
+    },
+  ];
+  return <div>
+    sdsadsd
+{/* <MaterialTable data={data} columns={columns}/> */}
 
-  // const tableInstance = useTable({ columns, data });
-  // const[getTableProps,getTableBodyProps,headerGroups,rows,prepareRow]=tableInstance;
-  // return <Table {...getTableBodyProps}>
-  //   <TableHead>
-  //     {headerGroups.map(headerGroup=>)}
-  //   </TableHead>
-  // </Table>
-  <div></div>;
+  </div>;
 }
 
 export default Details;
