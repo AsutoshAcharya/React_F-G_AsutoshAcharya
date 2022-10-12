f921447a2f0a848c89905f74116f4c84a99efad0
Asutosh Acharya <asutosh246@gmail.com>
2022-10-12 19:29:51 +0530

    filtering table content depending on your input

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -6,8 +6,9 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import DetailsHeader from "../DetailsHeader/DetailsHeader";
-
+import { useState } from "react";
 function Details() {
+  const [searchTerm, setSearchTerm] = useState("");
   function allStorage() {
     var values = [],
       keys = Object.keys(localStorage),
@@ -21,56 +22,76 @@ function Details() {
   }
   const rows = allStorage();
 
+  const getData = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  console.log(searchTerm);
+
   return (
     <div>
-      <DetailsHeader data={rows}/>
-    <TableContainer component={Paper} >
-      <Table sx={{ width:'150%', height: "40vh" }} aria-label="simple table">
-        <TableHead>
-          <TableRow sx={{backgroundColor:'rgb(239, 233, 251)'}}>
-            <TableCell align="center">
-              <input type="checkbox" name="" id="" />
-            </TableCell>
-            <TableCell align="center">Form Details</TableCell>
-            <TableCell align="center">Customer Name</TableCell>
-            <TableCell align="center">Email</TableCell>
-            <TableCell align="center">Phone</TableCell>
-            <TableCell align="center">
-              Please rate the quality of service you received from host
-            </TableCell>
-            <TableCell align="center">
-              Please rate the quality of beverage
-            </TableCell>
-            <TableCell align="center">
-              Please rate the quality of overall dining experience
-            </TableCell>
-            <TableCell align="center">Was our restaurant clean?</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell>
+      <DetailsHeader data={rows} getData={getData} />
+      <TableContainer component={Paper}>
+        <Table sx={{ width: "150%", height: "40vh" }} aria-label="simple table">
+          <TableHead>
+            <TableRow sx={{ backgroundColor: "rgb(239, 233, 251)" }}>
+              <TableCell align="center">
                 <input type="checkbox" name="" id="" />
               </TableCell>
-              <TableCell sx={{color:''}}>View Details</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell component="th" scope="row">
-                {row.email}
+              <TableCell align="center">Form Details</TableCell>
+              <TableCell align="center">Customer Name</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">Phone</TableCell>
+              <TableCell align="center">
+                Please rate the quality of service you received from host
               </TableCell>
-              <TableCell align="center">{row.phone}</TableCell>
-              <TableCell align="center">{row.qualityOfService}</TableCell>
-              <TableCell align="center">{row.beverage}</TableCell>
-              <TableCell align="center">{row.diningExperience}</TableCell>
-              <TableCell align="center">{row.cleanliness}</TableCell>
+              <TableCell align="center">
+                Please rate the quality of beverage
+              </TableCell>
+              <TableCell align="center">
+                Please rate the quality of overall dining experience
+              </TableCell>
+              <TableCell align="center">Was our restaurant clean?</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows
+              .filter((val) => {
+                if (searchTerm === "") {
+                  return val;
+                }
+                 else if (
+                  val.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                  val.phone.includes(searchTerm) ||
+                  val.email.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                  return val;
+                }
+              })
+              .map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>
+                    <input type="checkbox" name="" id="" />
+                  </TableCell>
+                  <TableCell sx={{ color: "" }} align="center">
+                    View Details
+                  </TableCell>
+                  <TableCell align="center">{row.name}</TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.email}
+                  </TableCell>
+                  <TableCell align="center">{row.phone}</TableCell>
+                  <TableCell align="center">{row.qualityOfService}</TableCell>
+                  <TableCell align="center">{row.beverage}</TableCell>
+                  <TableCell align="center">{row.diningExperience}</TableCell>
+                  <TableCell align="center">{row.cleanliness}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
--- src/components/DetailsHeader/DetailsHeader.js
+++ src/components/DetailsHeader/DetailsHeader.js
@@ -2,18 +2,33 @@ import React from "react";
 import "./DetailsHeader.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import RefreshIcon from "@mui/icons-material/Refresh";
+
 function DetailsHeader(props) {
+  // const [searchTerm, setSearchTerm] = useState("");
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+
   return (
     <div className="heading">
-     <div>
-     <h3 className="h3">AromaticBar</h3>
-      {props.data.length} records found
-     </div>
+      <div>
+        <h3 className="h3">AromaticBar</h3>
+        {props.data.length} records found
+      </div>
       <div className="input-icons">
-        <input type="text" name="" id="" className="input-field"/>
+        <input
+          type="text"
+          name=""
+          id=""
+          className="input-field"
+          placeholder="Search..."
+          onChange={props.getData}
+        />
         <SearchRoundedIcon className="icon" />
-        <button className="refresh">
-          <RefreshIcon className="refresh"/>
+        <button
+          className="refresh"
+          onClick={refreshPage}>
+          <RefreshIcon className="refresh" />
         </button>
         <button className="add-new">Add New</button>
       </div>
